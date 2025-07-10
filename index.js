60cf6c6d041c70c8416bb851695733b6820fbba2
rajvichauhan77 <chauhanrajvi77@gmail.com>
2025-07-10 13:25:27 +0530

    ewrkfg

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.set("view engine", "ejs")
 app.use(express.urlencoded({ extended: true }))
 
 app.use("/", express.static(path.join(__dirname, "public")))
+
+// app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 
 app.use("/", require("./routes/admin"))
