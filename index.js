bccbafdd0c2aa3fa6daeefa5fb044ae87f5bb18c
rajvichauhan77 <chauhanrajvi77@gmail.com>
2025-07-12 12:51:01 +0530

    new change

--- index.js
+++ index.js
@@ -12,13 +12,9 @@ app.set("view engine", "ejs")
 
 app.use(express.urlencoded({ extended: true }))
 
-<<<<<<< HEAD
-app.use("/", express.static(path.join(__dirname, "/public")))
-=======
 app.use("/", express.static(path.join(__dirname, "public")))
 
 // app.use('/dist', express.static(path.join(__dirname, 'dist')));
->>>>>>> 60cf6c6d041c70c8416bb851695733b6820fbba2
 app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 
 app.use("/", require("./routes/admin"))
